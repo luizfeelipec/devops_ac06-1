259af9f113f732911a8abbd1bcee7f40459b66d8
luizfeelipec <luizfeelipec1@hotmail.com>
2019-09-27 21:28:38 -0300

    Update contaCorrente.py

--- com/kuma/contaCorrente.py
+++ com/kuma/contaCorrente.py
@@ -1,32 +1,33 @@
 class ContaCorrente:
-	'''
-	classe para conta corrente
-	'''
-	def __init__(self, numero, nomeCorrentista, saldo = 0.0):		
-	self.numero = numero
-        self.alterar_Nome(nomeCorrentista)
+    '''
+    classe para conta corrente
+    '''
+
+    def __init__(self, numero, nomecorrentista, saldo=0.0):
+        self.numero = numero
+        self.alterar_nome(nomecorrentista)
         self.saldo = saldo
 
-    def alterar_Nome(self, nomeCorrentista):
-		'''
-		metodo parar o nome 
-		'''
-        self.nomeCorrentista = nomeCorrentista
+    def alterar_nome(self, nomecorrentista):
+        '''
+        metodo parar o nome
+        '''
+        self.nomecorrentista = nomecorrentista
 
     def deposito(self, valor):
-		'''
-		metodo para fazer deposito
-		'''
+        '''
+        metodo para fazer deposito
+        '''
         self.saldo += valor
 
     def saque(self, valor):
-		'''
-		metodo para fazer saque
-		'''
+        '''
+        metodo para fazer saque
+        '''
         self.saldo -= valor
 
-# TESTE DA CLASSE
 
+# TESTE DA CLASSE
 '''conta = ContaCorrente(1222, 'pablo')
 conta.alterarNome('julio')
 conta.deposito(100)
